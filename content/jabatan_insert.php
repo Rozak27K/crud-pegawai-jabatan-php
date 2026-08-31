48f900ce44827b41b2ae50c3383e26f994558bab
Rozak27K <rozak7272277@gmail.com>
2026-08-31 18:14:25 +0700

    Hapus file template yang tidak digunakan

--- content/jabatan_insert.php
+++ content/jabatan_insert.php
@@ -1,23 +1,21 @@
 <?php
 global $con;
-if(!defined('INDEX')) die("");
+if (!defined('INDEX')) die("");
 
-if(empty($_POST['nama'])){
-    echo"Nama jabatan tidak boleh kosong";
+if (empty($_POST['nama'])) {
+    echo "Nama jabatan tidak boleh kosong";
     header("refresh:1;url=?hal=jabatan_tambah");
     exit;
 }
 
-$query =mysqli_query($con , "INSERT INTO jabatan SET nama_jabatan ='$_POST[nama]'");
+$query = mysqli_query($con, "INSERT INTO jabatan SET nama_jabatan ='$_POST[nama]'");
 
-if($query){
-    echo"Data berhasil disimpan";   
+if ($query) {
+    echo "Data berhasil disimpan";
     header("refresh:1;url=?hal=jabatan");
     exit;
-}else{
-    echo"Tidak dapat menyimpan data";
+} else {
+    echo "Tidak dapat menyimpan data";
     echo mysqli_error($con);
     header("refresh:1;url=?hal=jabatan");
 }
-
-?>
